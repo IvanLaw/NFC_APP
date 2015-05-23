3fc1176c114bf692846669deec3ea1716a9d2a8a
IvanLaw <ivancylaw@gmail.com>
2015-05-23 20:44:14 +0800

    Initial commit

---
+++ app/src/main/java/com/fyp/nfc/nfcapp/main.java
@@ -0,0 +1,58 @@
+package com.fyp.nfc.nfcapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class main extends Activity {
+
+    public void beginActivity(View v){
+        Intent i = null;
+        switch(v.getId()){
+            case R.id.button1:
+                i = new Intent(this, intro.class);
+                break;
+            case R.id.button2:
+                i = new Intent(this, wifi.class);
+                break;
+            case R.id.button3:
+                i = new Intent(this, scan.class);
+                break;
+            case R.id.button4:
+                i = new Intent(this, info.class);
+                break;
+        }
+
+        startActivity(i);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
