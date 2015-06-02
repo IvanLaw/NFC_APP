cc846266c8d3bea1bc476a0d12857216d84a0cd3
IvanLaw <ivancylaw@gmail.com>
2015-06-03 02:08:47 +0800

    updated wi-fi activity

--- app/src/main/java/com/fyp/nfc/nfcapp/wifi.java
+++ app/src/main/java/com/fyp/nfc/nfcapp/wifi.java
@@ -1,36 +1,58 @@
 package com.fyp.nfc.nfcapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class wifi extends Activity {
 
+    private TextView mTextView;
+    private String wifi_SSID = "\"CityU WLAN (WPA)\"";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi);
-    }
+        mTextView = (TextView)findViewById(R.id.tv);
+        checkWifiState(this, wifi_SSID);
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.wifi, menu);
-        return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
+    protected void onResume() {
+        super.onResume();
+        checkWifiState(this, wifi_SSID);
+    }
+
+    public boolean checkWifiState(Context context, String wifi_SSID){
+        try {
+            WifiManager mWifiManager=(WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo=mWifiManager.getConnectionInfo();
+            if (!mWifiManager.isWifiEnabled() || wifiInfo.getSSID() == null) {
+                mTextView.setText("Wi-Fi is not enabled");
+                return false;
+            }
+
+            if (!wifiInfo.getSSID().equals(wifi_SSID)) {
+                mTextView.setText("Please connect our Wi-Fi: " + wifi_SSID);
+                return false;
+            }
+
+            mTextView.setText("Wi-Fi is connected");
             return true;
         }
-        return super.onOptionsItemSelected(item);
+        catch (  Exception e) {
+            return false;
+        }
     }
+
+
+
 }
